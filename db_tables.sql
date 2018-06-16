1ca7b6a48bf8ecabbb1ed101fb9b6b24502cb4d1
Julio B <julio.bacel@gmail.com>
2018-06-16 21:27:50 +0300

    Update DB design

--- db_tables.sql
+++ db_tables.sql
@@ -1,40 +1,77 @@
-CREATE TABLE professor (
+CREATE TYPE user_role AS ENUM ('professor', 'student');
+
+CREATE TABLE users (
 	id serial primary key,
 	username varchar(32) unique not null,
 	password varchar(128) not null,
+	role user_role not null
+);
+
+CREATE TABLE professors (
+	id int unique references users(id),
 	first_name varchar(40) not null,
 	last_name varchar(40) not null
 );
 
-CREATE TABLE assignment (
+CREATE TABLE students (
+	id int unique references users(id),
+	first_name varchar(40) not null,
+	last_name varchar(40) not null
+);
+
+CREATE TABLE groups (
+	id serial primary key
+);
+
+CREATE TABLE group_members (
+	group_id int references groups(id),
+	student_id int references students(id)
+);
+
+CREATE TABLE assignments (
 	id serial primary key,
 	title varchar(50) not null,
+	description varchar(200),
+	professor_id int references professors(id),
 	max_grade int not null,
-    max_group_size int,
-    description bytea,
-    ext varchar(5)
+	max_group_size int
+	--create_date date,
+	--due_date date,
 );
 
-CREATE TABLE a_owner (
-	p_id int references professor(id),
-    a_id int references assignment(id)
+CREATE TABLE assignment_groups (
+	assignment_id int references assignments(id),
+	group_id int references groups(id),
+	file bytea,
+	grade int
 );
 
-CREATE TABLE student (
-	id serial primary key,
-	username varchar(32) unique not null,
-	password varchar(128) not null,
-	first_name varchar(40) not null,
-	last_name varchar(40) not null
-);
 
-CREATE TABLE student_group (
-	id serial primary key,
-    s_id int references student(id)
-);
+-----testing
+INSERT INTO users (username, password, role) VALUES
+('p01102', 'tawa', 'student'),
+('p23201', 'tawa', 'student'),
+('p99101', 'tswa', 'student'),
+('drllll', 'stps', 'professor'),
+('testAM', 'pplp', 'professor');
 
-CREATE TABLE a_groups (
-	a_id int references assignment(id),
-    g_id int references student_group(id),
-    grade int
-);
+INSERT INTO professors (id, first_name, last_name) VALUES
+(4, 'Test', 'Professors'),
+(5, 'Tset', 'Prof');
+
+INSERT INTO students (id, first_name, last_name) VALUES
+(1, 'Stud', 'Ent'),
+(2, 'LLLL', 'lllll'),
+(3, 'terte', 'papsda');
+
+INSERT INTO groups (id) VALUES (1); --new group
+INSERT INTO group_members (group_id, student_id) VALUES (1, 1), (1, 3);
+INSERT INTO groups (id) VALUES (2); --new group
+INSERT INTO group_members (group_id, student_id) VALUES (2, 2);
+
+
+INSERT INTO assignments (title, professor_id, max_grade) VALUES ('Texn Log', 4, 10);
+INSERT INTO assignments (title, professor_id, max_grade) VALUES ('Vaseis ded', 5, 5);
+
+INSERT INTO assignment_groups (assignment_id, group_id, grade) VALUES (1, 1, 8);
+INSERT INTO assignment_groups (assignment_id, group_id, grade) VALUES (2, 2, 10);
